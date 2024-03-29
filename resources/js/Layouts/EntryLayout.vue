e9ebbdf7731f8e032df528b7495746fb60c40a32
OLiviER <olivier.bruch@gmail.com>
2024-03-29 11:58:19 +0100

    added entry layout

---
+++ resources/js/Layouts/EntryLayout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Card from '../Components/Generic/Card.vue'
+
+const { title } = defineProps({
+    title: {
+        type: String,
+        default: 'Welcome to Tribe'
+    }
+})
+</script>
+
+<template>
+    <div class="flex h-full w-full p-1 gap-1">
+        <div class="flex-grow">
+            <slot name="ad" />
+        </div>
+
+        <Card styles="w-2/5 max-w-xl h-full flex flex-col" padding="p-2">
+            <div class="flex justify-center my-2">
+                <img src="/logo.svg" alt="logo" class="w-10" />
+            </div>
+            <h2 class="text-center text-2xl font-bold">{{ title }}</h2>
+
+            <slot />
+        </Card>
+    </div>
+</template>
