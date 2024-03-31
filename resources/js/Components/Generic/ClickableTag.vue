57c9de0eea172e664ef809d626604db2d1ea79b9
OLiviER <olivier.bruch@gmail.com>
2024-03-31 18:47:36 +0200

    added clickable tag component

---
+++ resources/js/Components/Generic/ClickableTag.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    selected: Boolean,
+    onSelect: Function,
+    color: {
+        type: String,
+        default: 'secondary'
+    },
+    size: {
+        type: String,
+        default: 'md'
+    },
+    styles: {
+        type: String,
+        default: ''
+    }
+})
+
+const textSizeClass = computed(() => `text-${props.size}`)
+const borderColorClass = computed(() => `border-${props.color}`)
+const bgColorClass = computed(() => `bg-${props.color}`)
+const textColorClass = computed(() => `text-${props.color}`)
+const hoverBgColorClass = computed(() => `${props.color}/10`)
+</script>
+
+<template>
+    <button 
+        @click="onSelect" 
+        :class="`py-1 px-3 rounded-full border-2 duration-100 text-center ${textSizeClass} ${borderColorClass} ${selected ? bgColorClass + ' text-black' : 'bg-transparent ' + textColorClass + ' hover:bg-' + hoverBgColorClass}` + ' ' + styles"
+    >
+        <slot />
+    </button>
+</template>
--- resources/js/Pages/Auth/SetInterests.vue
+++ resources/js/Pages/Auth/SetInterests.vue
@@ -3,19 +3,32 @@ import EntryLayout from '@/Layouts/EntryLayout.vue'
 import AvatarInput from '@/Components/Generic/AvatarInput.vue'
 import Stepper from '@/Components/Generic/Stepper.vue'
 import { useForm, usePage } from '@inertiajs/vue3'
-import { DEFAULT_PROFILE_PIC, REGISTER_STEPS } from '@/lib/constants'
+import { REGISTER_STEPS } from '@/lib/constants'
 import Textarea from '@/Components/Generic/Textarea.vue'
 import Button from '@/Components/Generic/Button.vue'
 import UsernameInput from '@/Components/Auth/UsernameInput.vue'
 import { ref } from 'vue'
+import ClickableTag from '../../Components/Generic/ClickableTag.vue'
 
 const { categories } = defineProps({
     categories: Array
 })
 
 const form = useForm({
-    
+    tags: []
 })
+
+const tagIsSelected = (tag) => {
+    return form.tags.includes(tag)
+}
+
+const handleTagSelect = (tag) => {
+    if (tagIsSelected(tag)) {
+        form.tags = form.tags.filter(t => t !== tag)
+    } else {
+        form.tags.push(tag)
+    }
+}
 
 const submit = () => {
     
@@ -28,6 +41,20 @@ const submit = () => {
             <Stepper :steps="REGISTER_STEPS" selected-step-value="interests" />
         </div>
 
-        
+        <div class="flex flex-col flex-1 h-0 overflow-auto px-2">
+            <div class="flex-grow flex flex-col gap-4">
+                <div v-for="category in categories" :key="category.uuid">
+                    <h2 class="text-xl mb-2">{{ category.name }}</h2>
+
+                    <div class="flex gap-y-4 gap-x-2 flex-wrap items-center">
+                        <div v-for="tag in category.tags" :key="tag">
+                            <ClickableTag :selected="tagIsSelected(tag)" :on-select="() => handleTagSelect(tag)">{{ tag }}</ClickableTag>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Button styles="w-full" type="submit">Complete</Button>
+        </div>
     </EntryLayout>
 </template>
