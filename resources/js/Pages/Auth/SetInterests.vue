87e742427cbb8ed09815fb2de0b95b2bf6544f05
OLiviER <olivier.bruch@gmail.com>
2024-03-31 17:23:59 +0200

    interests page layout

--- resources/js/Pages/Auth/SetInterests.vue
+++ resources/js/Pages/Auth/SetInterests.vue
@@ -1,7 +1,29 @@
 <script setup>
+import EntryLayout from '@/Layouts/EntryLayout.vue'
+import AvatarInput from '@/Components/Generic/AvatarInput.vue'
+import Stepper from '@/Components/Generic/Stepper.vue'
+import { useForm, usePage } from '@inertiajs/vue3'
+import { DEFAULT_PROFILE_PIC, REGISTER_STEPS } from '@/lib/constants'
+import Textarea from '@/Components/Generic/Textarea.vue'
+import Button from '@/Components/Generic/Button.vue'
+import UsernameInput from '@/Components/Auth/UsernameInput.vue'
+import { ref } from 'vue'
 
+const form = useForm({
+    
+})
+
+const submit = () => {
+    
+}
 </script>
 
 <template>
-    
+    <EntryLayout>
+        <div class="mt-4 mb-8">
+            <Stepper :steps="REGISTER_STEPS" selected-step-value="interests" />
+        </div>
+
+        
+    </EntryLayout>
 </template>
