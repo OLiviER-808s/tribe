46fe8f5afcc854d7ac2a906307a31cdce33c2aa4
OLiviER <olivier.bruch@gmail.com>
2024-03-31 17:43:37 +0200

    added tag category seeder

---
+++ app/Models/TagCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UsesUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+
+class TagCategory extends Model
+{
+    use HasFactory, UsesUuid, HasTags;
+
+    protected $fillable = [
+        'name'
+    ];
+}
