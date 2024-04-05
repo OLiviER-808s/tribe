98b87f6efe17ef83b0c9e4d5a002d5ea98e67b87
OLiviER <olivier.bruch@gmail.com>
2024-04-05 20:42:26 +0200

    added tags list

---
+++ resources/js/Components/Generic/Tag.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    color: {
+        type: String,
+        default: 'secondary'
+    },
+    size: {
+        type: String,
+        default: 'md'
+    },
+    styles: {
+        type: String,
+        default: ''
+    }
+})
+</script>
+
+<template>
+    <div :class="`py-1 px-4 rounded-full border-2 duration-100 text-center text-${size} bg-${color}`">
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Settings/Account.vue
+++ resources/js/Pages/Settings/Account.vue
@@ -4,6 +4,7 @@ import { ModalsContainer, useModal } from 'vue-final-modal'
 import Button from '@/Components/Generic/Button.vue'
 import ProfileEditModal from '@/Components/Modals/ProfileEditModal.vue'
 import ProfileCard from '@/Components/Profile/ProfileCard.vue'
+import Tag from '@/Components/Generic/Tag.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
 
@@ -22,16 +23,36 @@ const { open, close } = useModal({
         <section class="flex justify-center pt-8">
             <ModalsContainer />
 
-            <div class="w-full max-w-2xl p-2">
-                <div class="flex items-center justify-between mb-4">
-                    <h2 class="text-2xl font-medium">Profile</h2>
+            <div class="w-full max-w-2xl p-2 flex flex-col gap-12">
+                <!-- Profile -->
+                <div>
+                    <div class="flex items-center justify-between mb-4">
+                        <h2 class="text-2xl font-medium">Profile</h2>
 
-                    <Button :on-click="open">
-                        <FontAwesomeIcon :icon="faPencil" /> Edit
-                    </Button>
+                        <Button :on-click="open">
+                            <FontAwesomeIcon :icon="faPencil" /> Edit
+                        </Button>
+                    </div>
+
+                    <ProfileCard :profile="$page.props.profile" />
                 </div>
 
-                <ProfileCard :profile="$page.props.profile" />
+                <!-- Interests -->
+                <div>
+                    <div class="flex items-center justify-between mb-4">
+                        <h2 class="text-2xl font-medium">Interests</h2>
+
+                        <Button :on-click="open">
+                            <FontAwesomeIcon :icon="faPencil" /> Edit
+                        </Button>
+                    </div>
+
+                    <div class="flex gap-2 items-center flex-wrap">
+                        <div v-for="interest in $page.props.profile.interests" :key="interest">
+                            <Tag>{{ interest }}</Tag>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </SettingsLayout>
