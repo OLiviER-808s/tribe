a8aa498387f0d0482cb9f52a9cd597836e0aafa4
OLiviER <olivier.bruch@gmail.com>
2024-04-23 09:48:02 +0100

    added attachment drawer

---
+++ resources/js/Components/Chat/MessageAttachmentSelect.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Button from '../Generic/Button.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCamera, faFileLines, faHeadphones, faPhotoFilm, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+const { show } = defineProps({
+    show: Boolean
+})
+</script>
+
+<template>
+    <div v-if="show" class="flex items-center justify-center gap-4 flex-wrap mb-4">
+        <Button variant="outline" styles="w-24">
+            <div>
+                <FontAwesomeIcon :icon="faCamera" />
+                <p class="text-sm">Camera</p>
+            </div>
+        </Button>
+
+        <Button variant="outline" color="secondary" styles="w-24">
+            <div>
+                <FontAwesomeIcon :icon="faPhotoFilm" />
+                <p class="text-sm">Gallery</p>
+            </div>
+        </Button>
+
+        <Button variant="outline" color="accent" styles="w-24">
+            <div>
+                <FontAwesomeIcon :icon="faHeadphones" />
+                <p class="text-sm">Audio</p>
+            </div>
+        </Button>
+
+        <Button variant="outline" color="success" styles="w-24">
+            <div>
+                <FontAwesomeIcon :icon="faFileLines" />
+                <p class="text-sm">Files</p>
+            </div>
+        </Button>
+
+        <Button variant="outline" color="error" styles="w-24">
+            <div>
+                <FontAwesomeIcon :icon="faLocationDot" />
+                <p class="text-sm">Location</p>
+            </div>
+        </Button>
+    </div>
+</template>
--- resources/js/Components/InputFields/MessageInput.vue
+++ resources/js/Components/InputFields/MessageInput.vue
@@ -2,15 +2,18 @@
 import Textbox from '../Generic/Textbox.vue'
 import IconButton from '../Generic/IconButton.vue'
 import { ref } from 'vue'
-import { faCamera, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faPaperclip } from '@fortawesome/free-solid-svg-icons'
 import { router, usePage } from '@inertiajs/vue3'
 import { inject } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
+import MessageAttachmentSelect from '../Chat/MessageAttachmentSelect.vue'
 
 const { chat, activeMembers } = defineProps({
 	chat: Object,
 	activeMembers: Array,
 })
+
+const attachmentDrawOpen = ref(false)
 
 const content = ref('')
 const typing = ref(false)
@@ -67,17 +70,21 @@ const startTyping = () => {
 </script>
 
 <template>
-    <form @submit.prevent="send()" class="flex items-center gap-2 w-full p-2 bg-inherit">
-		<div class="flex-grow">
-			<Textbox variant="outline" placeholder="Message" v-model="content" :on-input="startTyping">
-				<template #right-section>
-					<div class="mr-1 flex items-center justify-center">
-						<IconButton :icon="faCamera" color="base" variant="subtle" />
-					</div>
-				</template>
-			</Textbox>
-		</div>
+    <div class="p-2 bg-inherit w-full">
+		<MessageAttachmentSelect :show="attachmentDrawOpen" />
 
-		<IconButton :icon="faPaperPlane" size="lg" type="submit" />
-	</form>
+		<form @submit.prevent="send()" class="flex items-center gap-2">
+			<div class="flex-grow">
+				<Textbox variant="outline" placeholder="Message" v-model="content" :on-input="startTyping">
+					<template #right-section>
+						<div class="mr-1 flex items-center justify-center">
+							<IconButton :icon="faPaperclip" color="base" variant="subtle" @click="attachmentDrawOpen = !attachmentDrawOpen" />
+						</div>
+					</template>
+				</Textbox>
+			</div>
+
+			<IconButton :icon="faPaperPlane" size="lg" type="submit" />
+		</form>
+	</div>
 </template>
