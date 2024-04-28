f2e22d44a146fd7af64a4ebdb91a2dd536bcb652
OLiviER <olivier.bruch@gmail.com>
2024-04-28 17:24:39 +0100

    grid layout

--- resources/js/Components/Chat/MessageAttachments.vue
+++ resources/js/Components/Chat/MessageAttachments.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { faFile } from '@fortawesome/free-solid-svg-icons'
+import { faFile, faHeadphones, faVideo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { useFiles } from '@/Composables/useFiles'
 
@@ -11,7 +11,18 @@ const { readableFileSize } = useFiles()
 </script>
 
 <template>
-    <div v-if="files.length > 0" class="flex flex-col mb-1 gap-2">
+    <div v-if="files.length > 3" class="grid grid-cols-2 mb-1 gap-1">
+        <button v-for="(file, idx) in files.slice(0, 4)" class="xl:w-32 xl:h-32 lg:w-28 lg:h-28 w-24 h-24">
+            <img v-if="file.type === 'image'" :src="file.preview" class="xl:w-32 xl:h-32 lg:w-28 lg:h-28 w-24 h-24 rounded-sm object-cover" />
+            <div v-else class="rounded-sm bg-card flex justify-center items-center text-base-text h-full w-full">
+                <h4 v-if="files.length > 4 && idx === 3" class="text-xl font-semibold">+{{ files.length - 3 }}</h4>
+                <FontAwesomeIcon v-else-if="file.type === 'audio'" :icon="faHeadphones" size="xl" />
+                <FontAwesomeIcon v-else-if="file.type === 'video'" :icon="faVideo" size="xl" />
+                <FontAwesomeIcon v-else :icon="faFile" size="xl" />
+            </div>
+        </button>
+    </div>
+    <div v-else-if="files.length > 0" class="flex flex-col mb-1 gap-2">
         <div v-for="file in files">
             <img v-if="file.type === 'image'" :src="file.preview" class="rounded-sm" />
             <audio v-else-if="file.type === 'audio'" :src="file.preview" controls />
