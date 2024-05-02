55378ff91138fdf307c8ba3fc91216196b4e6801
OLiviER <olivier.bruch@gmail.com>
2024-05-02 15:56:37 +0100

    user can open and close chat list sidebar

--- resources/js/Components/Display/ChatHeader.vue
+++ resources/js/Components/Display/ChatHeader.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { faArrowLeft, faPhone, faVideoCamera } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faPhone, faVideoCamera } from '@fortawesome/free-solid-svg-icons'
 import IconButton from '../Generic/IconButton.vue'
 import { ref, watch } from 'vue'
-import { usePage } from '@inertiajs/vue3'
+import { router, usePage } from '@inertiajs/vue3'
 import Badge from '../Generic/Badge.vue'
 import ChatOptions from '@/Components/Dropdowns/ChatOptions.vue'
 import { useIsHandheld } from '@/Composables/useIsHandheld'
+import { inject } from 'vue'
 
 const props = defineProps({
 	chat: Object,
 	activeMembers: Array
 })
+
+const showChatSidebar = inject('showChatSidebar')
 
 const page = usePage()
 const { isHandheld } = useIsHandheld()
@@ -18,22 +21,28 @@ const { isHandheld } = useIsHandheld()
 const otherMembers = ref([])
 const typingMembers = ref([])
 
-watch(() => props.activeMembers, (newMembers, oldMembers) => {
-    const authMember = props.chat.members.find(member => member.user_uuid === page.props.profile.uuid);
+const toggleSidebar = () => {
+	if (isHandheld.value) return router.get(route('chats'))
+
+	showChatSidebar.value = !showChatSidebar.value
+}
+
+watch(() => props.activeMembers, (newMembers) => {
+    const authMember = props.chat.members.find(member => member.user_uuid === page.props.profile.uuid)
     
-    otherMembers.value = newMembers.filter(member => member.uuid !== authMember.uuid);
-    typingMembers.value = otherMembers.value.filter(member => member.typing);
+    otherMembers.value = newMembers.filter(member => member.uuid !== authMember.uuid)
+    typingMembers.value = otherMembers.value.filter(member => member.typing)
 }, { deep: true, immediate: true })
 </script>
 
 <template>
     <div class="p-2 flex items-center gap-2">
-		<div class="flex items-center" v-if="isHandheld">
-			<IconButton variant="subtle" color="base" :icon="faArrowLeft" size="lg" :href="route('chats')" />
+		<div class="flex items-center">
+			<IconButton variant="subtle" color="base" :icon="showChatSidebar ? faArrowLeft : faArrowRight" size="lg" :on-click="toggleSidebar" />
 		</div>
 		
 		<div class="font-medium">
-			<h2>{{ chat.name }}</h2>
+			<h2 class="mb-1">{{ chat.name }}</h2>
 
 			<div class="text-xs font-medium text-secondary-text flex items-center gap-1" v-if="otherMembers.length > 0">
 				<Badge color="success" styles="mt-0.5" />
--- resources/js/Layouts/ChatLayout.vue
+++ resources/js/Layouts/ChatLayout.vue
@@ -2,18 +2,21 @@
 import Card from '../Components/Generic/Card.vue'
 import AuthLayout from './AuthLayout.vue'
 import ChatList from '../Components/Navigation/ChatList.vue'
-import { provide, computed } from 'vue'
+import { provide, computed, ref } from 'vue'
 import { usePage } from '@inertiajs/vue3'
 
-provide('headerTitle', 'Chats')
+const showChatSidebar = ref(true)
 
 const page = usePage()
 const isIndexPage = computed(() => page.url === '/chats')
+
+provide('headerTitle', 'Chats')
+provide('showChatSidebar', showChatSidebar)
 </script>
 
 <template>
     <AuthLayout :show-mobile-footer="isIndexPage" :show-mobile-header="isIndexPage">
-        <template #additional-sidebar>
+        <template #additional-sidebar v-if="showChatSidebar">
             <Card styles="w-72 h-full" padding="p-1">
                 <ChatList />
             </Card>
