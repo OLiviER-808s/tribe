4b5ae4e37697a5e727f04a23d8988c69f786039b
OLiviER <olivier.bruch@gmail.com>
2024-05-05 09:36:17 +0100

    added quick filters component

---
+++ resources/js/Components/Discover/QuickFilters.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex items-center gap-2 mb-6">
+        <button class="filter bg-neutral-400 hover:bg-neutral-800 hover:text-dropdown-text">
+            test
+        </button>
+        <button class="filter selected-filter">
+            test 2
+        </button>
+    </div>
+</template>
+
+<style>
+    .filter {
+        @apply rounded-md duration-300 font-medium py-1 px-4;
+    }
+    .selected-filter {
+        @apply bg-neutral-800 text-dropdown-text;
+    }
+</style>
--- resources/js/Pages/Discover.vue
+++ resources/js/Pages/Discover.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthLayout from '@/Layouts/AuthLayout.vue'
 import Conversation from '@/Components/Display/Conversation.vue'
+import QuickFilters from '@/Components/Discover/QuickFilters.vue'
 import { provide, ref } from 'vue'
 import { useInfiniteScroll } from '@vueuse/core/index.cjs'
 import axios from 'axios'
@@ -31,14 +32,7 @@ useInfiniteScroll(conversationContainer, async () => {
         <div class="overflow-auto h-full">
             <section class="flex justify-center overflow-auto h-full" ref="conversationContainer">
                 <div class="px-1 py-6 w-full max-w-2xl">
-                    <div class="flex items-center gap-2 mb-6">
-                        <div class="cursor-pointer rounded-md bg-neutral-400 hover:bg-neutral-800 hover:text-dropdown-text duration-300 font-medium py-1 px-4">
-                            test
-                        </div>
-                        <div class="cursor-pointer rounded-md bg-neutral-800 text-dropdown-text font-medium py-1 px-4">
-                            test 2
-                        </div>
-                    </div>
+                    <QuickFilters />
 
                     <div class="flex flex-col gap-4">
                         <div v-for="conversation in conversations.data" :key="conversation.uuid">
