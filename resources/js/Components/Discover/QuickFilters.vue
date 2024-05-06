599cc155e4d6454021e108ab6791b3245d95081e
OLiviER <olivier.bruch@gmail.com>
2024-05-06 12:48:34 +0100

    styled quick filters

--- resources/js/Components/Discover/QuickFilters.vue
+++ resources/js/Components/Discover/QuickFilters.vue
@@ -1,23 +1,40 @@
 <script setup>
+import { ref } from 'vue'
 
+const selectedFilters = ref([])
 </script>
 
 <template>
-    <div class="flex items-center gap-2 mb-6">
-        <button class="filter bg-neutral-400 hover:bg-neutral-800 hover:text-dropdown-text">
-            test
+    <div class="filter-container">
+        <button class="filter" :class="{ 'selected-filter': selectedFilters.length < 1 }">
+            All
         </button>
-        <button class="filter selected-filter">
-            test 2
+
+        <button v-for="topic in $page.props.profile.interests" 
+        :key="topic.uuid"
+        class="filter" 
+        :class="{ 'selected-filter': selectedFilters.includes(topic.uuid) }">
+            {{ topic.label }}
         </button>
     </div>
 </template>
 
 <style>
     .filter {
-        @apply rounded-md duration-300 font-medium py-1 px-4;
+        @apply rounded-md duration-300 text-sm font-medium py-1 px-4 bg-neutral-400 text-nowrap;
     }
+    .filter:hover {
+        @apply bg-neutral-800 text-dropdown-text;
+    }
+
     .selected-filter {
         @apply bg-neutral-800 text-dropdown-text;
+    }
+
+    .filter-container {
+        @apply flex items-center gap-2 mb-4 pb-2 overflow-auto;
+    }
+    .filter-container::-webkit-scrollbar {
+        height: 6px;
     }
 </style>
